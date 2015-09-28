54a6661a597d9b697de60b3c328ee4ac1b471744
Joe Hilgard <jhilgard@asc.upenn.edu>
2015-09-28 17:02:31 -0400

    More analyses and visualization, plus a report .docx.

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(readxl)
 library(magrittr)
 library(lme4)
+library(arm)
 library(car)
 
 dat = read.delim("../data/FullData.txt", stringsAsFactors=F)
@@ -22,11 +23,20 @@ dat = dat %>%
   # Select and rename relevant columns
   select(
     # Session & Subject ID info
-    Subject, "Script" = ExperimentName, Condition, 
-    "Gender" = Gender.RESP.Block., Sex, "Race" = Race.RESP.Block., Subset,
-    mapping, laptop, "Date" = SessionDate, "Time" = SessionTime,
+    Subject, 
+    "Script" = ExperimentName, 
+    Condition, 
+    "Gender" = Gender.RESP.Block., 
+    Sex, 
+    "Race" = Race.RESP.Block., 
+    Subset,
+    mapping, 
+    laptop, 
+    "Date" = SessionDate, 
+    "Time" = SessionTime,
     # Block & Trial number info
-    "Block" = Procedure.Trial., "Trial" = SubTrial, 
+    "Block" = Procedure.Trial., 
+    "Trial" = SubTrial, 
     # Trial type & Stimulus info
     TrialType, CueType, ProbeType, BlackPrime, BlackTarget, GunTarget, WhitePrime, ToolTarget, 
     # Accuracy and RT info
@@ -63,7 +73,6 @@ table(dat$Subject, dat$TrialType) # Good!
 dat = dat %>%
   filter(!(Note %in% "BAD"))
 
-# Data exclusion for chance performance belongs here!
 # Count number of too-slows:
 temp = dat %>%
   filter(feedbackmask == "slow") %>%
@@ -90,6 +99,7 @@ meanAccTable = dat %>%
   group_by(Subject) %>%
   summarize("meanACC" = mean(Probe.ACC),
             "count" = n())
+hist(meanAccTable$meanACC); abline(v=.60, col="red")
 badSubs = meanAccTable %>%
   filter((meanACC < .608 & count == 120) | (meanACC < .615 & count==96)) %>%
   select(Subject) # doesn't matter whether p<.01 or p<.05
@@ -103,6 +113,8 @@ model1 = glmer(Probe.ACC ~ Condition * CueClass * Probe + (1|Subject),
                family = "binomial",
                data=datACC)
 Anova(model1, type=3)
+cbind("beta" = fixef(model1),
+      "se.beta" = se.fixef(model1))
 
 # Well, do we replicate standard WIT effect?
 model2 = 
@@ -112,19 +124,39 @@ datACC %>%
        family = "binomial",
        data = .)
 Anova(model2, type=3)
+cbind("beta" = fixef(model2),
+      "se.beta" = se.fixef(model2))
+
+# What happened in our new condition?
+model3 = 
+  datACC %>%
+  filter(Condition == "BlackGun") %>%
+  glmer(Probe.ACC ~ CueClass * Probe + (1|Subject),
+        family = "binomial",
+        data = .)
+Anova(model3, type=3)
+cbind("beta" = fixef(model3),
+      "se.beta" = se.fixef(model3))
 
 # Restrict analysis to just gun trials per prereg
-model3 = 
+model4 = 
   datACC %>%
   filter(ProbeClass == "WEAP") %>%
   glmer(Probe.ACC ~ Condition * CueClass + (1|Subject),
         family = "binomial",
         data = .)
-Anova(model3, type=3)
-fixef(model3)
+Anova(model4, type=3)
+cbind("beta" = fixef(model4),
+      "se.beta" = se.fixef(model4))
+
 
 # Well what's our damn sample size like?
 datACC  %>% distinct(Subject, Condition)  %$% table(Condition) # not great!
+dat  %>% 
+  filter(!(Subject %in% badSubs$Subject)) %>% 
+  group_by(Subject, Condition, TrialType) %>% 
+  summarize("count" = n()) %>% 
+  distinct(Subject, Condition, count)  %$% table(Condition, count) # not great!
 
 # Aggregate data for plotting
 # Consider that some TrialTypes appear in both conditions 
@@ -150,16 +182,75 @@ ggplot(plotDatACC, aes(x=interaction(CueClass, Probe), y=meanACC)) +
   geom_point() +
   facet_wrap(~Condition)
 
+ggplot(plotDatACC, aes(x=interaction(CueClass, Probe), y=meanACC)) +
+  geom_violin() +
+  geom_boxplot(width = .25, notch = T) +
+  facet_wrap(~Condition)
+
 plotDatACC %>%
   group_by(Condition, Probe, CueClass) %>%
   summarize("meanACC" = mean(meanACC)) %>%
+  ungroup() %>% 
+  mutate("Condition" = ifelse(Condition == "BlackGun", "Modified WIT", "Classic WIT")) %>%
   ggplot(., aes(x=interaction(CueClass, Probe), y=meanACC)) +
+  geom_bar(stat="identity") +
+  facet_wrap(~Condition) +
+  scale_y_continuous("Mean Accuracy", limits = c(0,1)) +
+  scale_x_discrete("Trial Type",
+                   labels=c("Black.Gun" = "Black-Gun",
+                            "white.Gun" = "White-Gun",
+                            "Black.Other" = "Black-Other",
+                            "white.Other" = "White-Other")) +
+  ggtitle("Task Accuracy")
+
+plotDatACC %>% 
+  filter(Condition == "GunTool") %>% 
+  ggplot(., aes(x=interaction(CueClass, Probe), y=meanACC)) +
+  #geom_violin() +
+  geom_boxplot(notch = T) +
+  scale_y_continuous("Mean accuracy", limits=c(.25, 1)) +
+  scale_x_discrete("Trial Type",
+                   labels=c("Black.Gun" = "Black-Gun",
+                            "white.Gun" = "White-Gun",
+                            "Black.Other" = "Black-Tool",
+                            "white.Other" = "White-Tool")) +
+  ggtitle("Classic WIT \n Black/White Primes, Gun/Tool Targets")
+
+plotDatACC %>% 
+  filter(Condition == "BlackGun") %>% 
+  ggplot(., aes(x=interaction(CueClass, Probe), y=meanACC)) +
+  #geom_violin() +
+  geom_boxplot(notch = T) +
+  scale_y_continuous("Mean accuracy", limits=c(.25, 1)) +
+  scale_x_discrete("Trial Type",
+                   labels=c("Black.Gun" = "Black-Gun",
+                            "white.Gun" = "White-Gun",
+                            "Black.Other" = "Black-Black",
+                            "white.Other" = "White-Black")) +
+  ggtitle("Modified WIT \n Black/White Primes, Black/Gun Targets")
+
+plotDatACC %>% 
+  ungroup() %>% 
+  mutate("Condition" = ifelse(Condition == "BlackGun", "Modified WIT", "Classic WIT")) %>% 
+  filter(Probe == "Gun") %>% 
+  ggplot(., aes(x=CueClass, y=meanACC)) +
+  facet_wrap(~Condition) +
+  #geom_violin() +
+  geom_boxplot(notch = T) +
+  scale_y_continuous("Mean accuracy", limits=c(.25, 1)) +
+  scale_x_discrete("Prime Type") +
+  ggtitle("Gun Targets Only")
+
+plotDatACC %>%
+  group_by(Condition, TrialType) %>%
+  summarize("meanACC" = mean(meanACC)) %>%
+  ggplot(., aes(x=TrialType, y=meanACC)) +
   geom_bar(stat="identity") +
   facet_wrap(~Condition)
 
 plotDatACC %>%
   group_by(Condition, TrialType) %>%
-  summarize("meanACC" = mean(meanACC)) %>%
+  summarize("meanACC" = weighted.mean(meanACC, count)) %>%
   ggplot(., aes(x=TrialType, y=meanACC)) +
   geom_bar(stat="identity") +
   facet_wrap(~Condition)
@@ -177,3 +268,11 @@ plotDatRT %>%
   ggplot(., aes(x=TrialType, y=meanRT)) +
   geom_bar(stat="identity") +
   facet_wrap(~Condition)
+
+plotDatACC %>% 
+  filter(Probe == "Gun") %>% 
+  group_by(Condition, TrialType) %>% 
+  summarize("meanACC" = mean(meanACC)) %>% 
+  ggplot(., aes(x=interaction(Condition, TrialType), y=meanACC)) +
+  geom_point(stat="identity", cex = 5) +
+  scale_y_continuous(limits = c(.7, 1))
